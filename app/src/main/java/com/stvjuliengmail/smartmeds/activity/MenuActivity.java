4ac7f1bf0246f153bbf88092406030b5cbfd2551
Steve Julien <stevenjulien@mail.weber.edu>
2018-02-04 15:23:55 -0700

    routes to mymeds and search activities

--- app/src/main/java/com/stvjuliengmail/smartmeds/activity/MenuActivity.java
+++ app/src/main/java/com/stvjuliengmail/smartmeds/activity/MenuActivity.java
@@ -1,15 +1,43 @@
 package com.stvjuliengmail.smartmeds.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.stvjuliengmail.smartmeds.R;
 
 public class MenuActivity extends AppCompatActivity {
 
+    Button btnSearchActivity;
+    Button btnMyMedsActivity;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        btnSearchActivity = (Button)findViewById(R.id.btnSearchActivity);
+        btnMyMedsActivity = (Button)findViewById(R.id.btnMyMedsActivity);
+
+        btnSearchActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startUp(SearchActivity.class);
+            }
+        });
+
+        btnMyMedsActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startUp(MyMedsActivity.class);
+            }
+        });
+    }
+
+    public void startUp(Class c) {
+        Intent intent = new Intent(this, c);
+        startActivity(intent);
     }
 }
