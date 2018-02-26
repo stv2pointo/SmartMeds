67a430712bd913b8429a15944756d41204f9937b
Steve Julien <stevenjulien@mail.weber.edu>
2018-02-26 16:14:31 -0700

    housekeeping

--- app/src/androidTest/java/com/stvjuliengmail/smartmeds/RxInfoUiTest.java
+++ app/src/androidTest/java/com/stvjuliengmail/smartmeds/RxInfoUiTest.java
@@ -6,25 +6,20 @@ import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 
 import com.stvjuliengmail.smartmeds.activity.RxInfoActivity;
-import com.stvjuliengmail.smartmeds.activity.SearchActivity;
 
 import org.junit.Rule;
 import org.junit.Test;
 
-import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
-import static android.support.test.espresso.action.ViewActions.typeText;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.CoreMatchers.equalTo;
 
 /**
  * Created by Steven on 2/26/2018.
  */
 
 public class RxInfoUiTest {
-    // creates activity before and destroys after
+    // creates activity  with intent extras before and destroys after
     @Rule
     public ActivityTestRule<RxInfoActivity> rxInfoActivityRule =
             new ActivityTestRule<RxInfoActivity>(RxInfoActivity.class) {
