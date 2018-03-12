e3686491c30e6ea62e90211c78ac800636fd1121
Steve Julien <stevenjulien@mail.weber.edu>
2018-03-12 13:56:12 -0600

    Menu aesthetics

--- app/src/main/java/com/stvjuliengmail/smartmeds/activity/MenuActivity.java
+++ app/src/main/java/com/stvjuliengmail/smartmeds/activity/MenuActivity.java
@@ -17,7 +17,8 @@ public class MenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-        this.deleteDatabase("SmartMeds.db");
+
+//        this.deleteDatabase("SmartMeds.db");
         btnSearchActivity = (Button)findViewById(R.id.btnSearchActivity);
         btnMyMedsActivity = (Button)findViewById(R.id.btnMyMedsActivity);
 
