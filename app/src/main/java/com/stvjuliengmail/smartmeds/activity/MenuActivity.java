a4eb7c7819d213cfe45883186841d7b67504cc9e
danmaloy <35476661+danmaloy@users.noreply.github.com>
2018-03-27 18:30:16 -0600

    Added disclaimer that generates when app is opened or button is clicked

--- app/src/main/java/com/stvjuliengmail/smartmeds/activity/MenuActivity.java
+++ app/src/main/java/com/stvjuliengmail/smartmeds/activity/MenuActivity.java
@@ -1,6 +1,8 @@
 package com.stvjuliengmail.smartmeds.activity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,15 +14,20 @@ public class MenuActivity extends AppCompatActivity {
 
     Button btnSearchActivity;
     Button btnMyMedsActivity;
+    Button btnDisclaimer;
+
+    boolean agreed = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
-//        this.deleteDatabase("SmartMeds.db");
+
+
         btnSearchActivity = (Button)findViewById(R.id.btnSearchActivity);
         btnMyMedsActivity = (Button)findViewById(R.id.btnMyMedsActivity);
+        btnDisclaimer = (Button)findViewById(R.id.btnDisclaimer);
 
         btnSearchActivity.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -35,6 +42,49 @@ public class MenuActivity extends AppCompatActivity {
                 startUp(MyMedsActivity.class);
             }
         });
+
+        btnDisclaimer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                disclaimer();
+            }
+        });
+
+        if (!agreed){
+            btnMyMedsActivity.setEnabled(false);
+            btnSearchActivity.setEnabled(false);
+            disclaimer();
+        }
+//        else if (!agreed) {
+//            btnSearchActivity.setOnClickListener(new View.OnClickListener() {
+//                @Override
+//                public void onClick(View v) {
+//                    disclaimer();
+//                }
+//            });
+//        }
+    }
+
+    private void disclaimer() {
+        AlertDialog alertDialog = new AlertDialog.Builder(this).create();
+        alertDialog.setTitle("Disclaimer");
+        alertDialog.setMessage("This app could kill you.  Are you ok with that?");
+        alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "CANCEL",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+        alertDialog.setButton(AlertDialog.BUTTON_POSITIVE, "AGREE",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                        agreed = true;
+                        btnMyMedsActivity.setEnabled(true);
+                        btnSearchActivity.setEnabled(true);
+                    }
+                });
+        alertDialog.show();
     }
 
     public void startUp(Class c) {
