e85b67bbffd9ebd4a9a93cd5920d60bb6ca2492d
danmaloy <35476661+danmaloy@users.noreply.github.com>
2018-03-29 15:16:38 -0600

    Saves to a SharedPreference if user agreed to disclaimer

--- app/src/main/java/com/stvjuliengmail/smartmeds/activity/MenuActivity.java
+++ app/src/main/java/com/stvjuliengmail/smartmeds/activity/MenuActivity.java
@@ -2,6 +2,7 @@ package com.stvjuliengmail.smartmeds.activity;
 
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,14 +17,10 @@ public class MenuActivity extends AppCompatActivity {
     Button btnMyMedsActivity;
     Button btnDisclaimer;
 
-    boolean agreed = false;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
-
-
 
         btnSearchActivity = (Button)findViewById(R.id.btnSearchActivity);
         btnMyMedsActivity = (Button)findViewById(R.id.btnMyMedsActivity);
@@ -50,25 +47,24 @@ public class MenuActivity extends AppCompatActivity {
             }
         });
 
-        if (!agreed){
+        if (!getAgreement()){
             btnMyMedsActivity.setEnabled(false);
             btnSearchActivity.setEnabled(false);
             disclaimer();
         }
-//        else if (!agreed) {
-//            btnSearchActivity.setOnClickListener(new View.OnClickListener() {
-//                @Override
-//                public void onClick(View v) {
-//                    disclaimer();
-//                }
-//            });
-//        }
     }
 
     private void disclaimer() {
         AlertDialog alertDialog = new AlertDialog.Builder(this).create();
         alertDialog.setTitle("Disclaimer");
-        alertDialog.setMessage("This app could kill you.  Are you ok with that?");
+        alertDialog.setMessage("The information in this app is not intended or implied to be a substitute for " +
+                "professional medical advice, diagnosis or treatment. All content, including text, graphics, " +
+                "images and information, contained on or available through this app is for general information purposes only. " +
+                "SmartMeds makes no representation and assumes no responsibility for the accuracy of information contained on " +
+                "or available through this app, and such information is subject to change without notice. You are encouraged to " +
+                "confirm any information obtained from or through this app with other sources, and review all information regarding " +
+                "any medical condition or treatment with your physician. NEVER DISREGARD PROFESSIONAL MEDICAL ADVICE OR DELAY SEEKING " +
+                "MEDICAL TREATMENT BECAUSE OF SOMETHING YOU HAVE READ ON OR ACCESSED THROUGH THIS APP.");
         alertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, "CANCEL",
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
@@ -79,12 +75,25 @@ public class MenuActivity extends AppCompatActivity {
                 new DialogInterface.OnClickListener() {
                     public void onClick(DialogInterface dialog, int which) {
                         dialog.dismiss();
-                        agreed = true;
+                        isAgreed(true);
                         btnMyMedsActivity.setEnabled(true);
                         btnSearchActivity.setEnabled(true);
                     }
                 });
         alertDialog.show();
+    }
+
+    private void isAgreed(boolean agreed){
+        SharedPreferences mSharedPreferences = getSharedPreferences("agreed", MODE_PRIVATE);
+        SharedPreferences.Editor mEditor = mSharedPreferences.edit();
+        mEditor.putBoolean("agreed", agreed);
+        mEditor.apply();
+    }
+
+    private boolean getAgreement(){
+        SharedPreferences mSharedPreferences = getSharedPreferences("agreed", MODE_PRIVATE);
+        boolean haveAgreed = mSharedPreferences.getBoolean("agreed", false);
+        return haveAgreed;
     }
 
     public void startUp(Class c) {
