05800fe2335f0071b424ed6b88571a8cb64d39a4
Tejas Shah <shahtejas15@gmail.com>
2018-04-05 21:25:20 -0600

    NAmES

--- app/src/main/java/com/stvjuliengmail/smartmeds/api/AutoFillTask.java
+++ app/src/main/java/com/stvjuliengmail/smartmeds/api/AutoFillTask.java
@@ -3,10 +3,17 @@ package com.stvjuliengmail.smartmeds.api;
 import android.app.ProgressDialog;
 import android.os.AsyncTask;
 import android.util.Log;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 import com.stvjuliengmail.smartmeds.activity.NameListActivity;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.lang.ref.WeakReference;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -43,58 +50,63 @@ public class AutoFillTask extends AsyncTask<String, Integer, String>
     protected String doInBackground(String... strings)
     {
         rawJson = "blah";
-//        try {
-//            URL url = new URL(buildRequest());
-//            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-//            connection.setRequestMethod("GET");
-//            connection.connect();
-//            int status = connection.getResponseCode();
-//            switch (status) {
-//                case 200:
-//                case 201:
-//                    BufferedReader br =
-//                            new BufferedReader(new InputStreamReader(connection.getInputStream()));
-//                    rawJson = br.readLine();
-//                    break;
-//                default:
-//                    Toast.makeText(weakActivity.get(), "Server Error", Toast.LENGTH_SHORT).show();
-//            }
-//        } catch (Exception e) {
-//            Log.d(TAG, "doInBackground() Exception !!: " + e.getMessage());
-//        }
+        try {
+            Log.d(TAG, "DoInBackground");
+            URL url = new URL(REQUEST_BASE.NAME_SUGGESTION);
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            int status = connection.getResponseCode();
+            switch (status) {
+                case 200:
+                case 201:
+                    BufferedReader br =
+                            new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                    rawJson = br.readLine();
+                    break;
+                default:
+                    Toast.makeText(weakActivity.get(), "Server Error", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            Log.d(TAG, "doInBackground() Exception !!: " + e.getMessage());
+        }
         return rawJson;
     }
 
     @Override
     protected void onPostExecute(String result) {
         super.onPostExecute(result);
+        GsonBuilder gsonB = new GsonBuilder();
+        Gson gson = gsonB.create();
 //        interactions = new ArrayList<>();
 //
-//        try {
-//            InteractionsResult resultObject = jsonParse(result);
-//            interactions = resultToInteractions(resultObject);
-//            disclaimer = resultObject.getNlmDisclaimer();
-//            if (interactions != null && interactions.size() > 0) {
-//                setResultsInUI();
-//            } else {
-//                Toast.makeText(weakActivity.get(), "No Results", Toast.LENGTH_SHORT).show();
-//            }
-//        } catch (Exception e) {
-//            Log.d(TAG, "onPostExecute: Exception !!!" + e.getMessage());
-//        }
+        try {
+            NameSearchResult resultObject;
+            resultObject = gson.fromJson(rawJson, NameSearchResult.class);
+            pillNamesFromAPI = resultToPillNames(resultObject);
+            if (pillNamesFromAPI != null && pillNamesFromAPI.size() > 0) {
+                setResultsInUI();
+            } else {
+                Toast.makeText(weakActivity.get(), "No Results", Toast.LENGTH_SHORT).show();
+            }
+        } catch (Exception e) {
+            Log.d(TAG, "onPostExecute: Exception !!!" + e.getMessage());
+        }
         if (progressDialog != null) {
             progressDialog.dismiss();
         }
         setResultsInUI();
     }
+
+    private ArrayList<String> resultToPillNames(NameSearchResult result)
+    {
+        ArrayList<String> list = new ArrayList<String>(Arrays.asList(result.getSuggestionGroup().getSuggestionList().getSuggestion()));
+        return list;
+    }
+
     public void setResultsInUI()
     {
-        pillNamesFromAPI = new ArrayList<String>(
-                Arrays.asList("Android", "iPhone", "WindowsMobile",
-                        "Blackberry", "WebOS", "Ubuntu", "Windows7", "Max OS X",
-                        "Linux", "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux",
-                        "OS/2", "Ubuntu", "Windows7", "Max OS X", "Linux", "OS/2",
-                        "Android", "iPhone", "WindowsMobile"));
+
         if (pillNamesFromAPI != null)
         {
             NameListActivity activity = weakActivity.get();
