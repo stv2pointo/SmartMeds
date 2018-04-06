97c9a0d29ca1c46ed54aac41d26744177670f899
Tejas Shah <shahtejas15@gmail.com>
2018-04-05 19:08:06 -0600

    Added adapter, path for name searching
    Not Done.

---
+++ app/src/main/java/com/stvjuliengmail/smartmeds/activity/NameListActivity.java
@@ -0,0 +1,15 @@
+package com.stvjuliengmail.smartmeds.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NameListActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_name_list);
+    }
+}
